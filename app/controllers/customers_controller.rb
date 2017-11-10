69f800331646cb2bcb53c2f3d7753ebe4f797cc9
joberton <joberton@academic.rrc.ca>
2017-11-10 02:25:44 +0000

    defined the routes and the actions assoicated with the customers controller for part 2 and added in some styling

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,13 @@
 class CustomersController < ApplicationController
+	def index
+		@customers = Customer.all
+	end
+
+	def alphabetized
+		@customers = Customer.all.order(:full_name);
+	end
+
+	def missingEmail
+		@customers = Customer.where(email_address: "")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'customers/missingEmail', to: 'customers#missingEmail', as: 'missingEmail'
+  get 'customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized'
+  root 'customers#index'
 end
